57b20566bffd952eb023d9825c9a1ecaa6813c5e
Myo Htut Kyaw <j25043740@student.newinti.edu.my>
2025-08-21 13:53:55 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -8,9 +8,5 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World");
-        System.out.println("My name is Steven!");
-		System.out.println("Course Name: Object-Oriented PROGRAMMING IN JAVA I");
-		System.out.println("Course Code: CSC200");
-		System.out.println("Lecturer Name: SATHISH SARMA");
     }
 }
